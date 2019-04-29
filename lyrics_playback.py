7dbd9b550191d867981f51fe084fd8072f13c67c
raphaelchang <raphaelchang1227@gmail.com>
2019-04-29 05:01:23 -0400

    add sleep

--- lyrics_playback.py
+++ lyrics_playback.py
@@ -3,6 +3,6 @@ import time
 def play(lyrics):
     start = time.time()
     for l in lyrics:
-        while time.time() - start  < l[1]:
-            pass
+        while time.time() - start < l[1]:
+            time.sleep(0.01)
         print(l[0])
